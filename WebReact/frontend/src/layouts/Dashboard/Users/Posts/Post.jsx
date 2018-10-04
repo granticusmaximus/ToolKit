960abb3ff03097d328f5a2a1090ea69cac344ee8
Grant Watson <grant@triumphdesigns.org>
2018-10-04 12:28:40 -0400

    Update to the Projects Component

--- WebReact/frontend/src/layouts/Dashboard/Users/Posts/Post.jsx
+++ WebReact/frontend/src/layouts/Dashboard/Users/Posts/Post.jsx
@@ -6,6 +6,11 @@ class Post extends Component {
         return (
             <div className="post">
                 <h2 className="post_title">{this.props.post.title}</h2>
+                <label>Person Assigned:</label>
+                <h2 className="post_other">{this.props.post.personAssigned}</h2>
+                <label>Due Date:</label>
+                <h2 className="post_other">{this.props.post.dueDate}</h2>
+                <label>Requirements:</label>
                 <p className="post_message">{this.props.post.message}</p>
                 <div className="control-buttons">
                     <button className="edit"
