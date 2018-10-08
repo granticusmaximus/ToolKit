7a029497eb3d45587ad18cc03436ef2dea667d00
Grant Watson <grant@triumphdesigns.org>
2018-10-08 16:34:32 -0400

    Addition of the Employee Service

---
+++ WebAPI/Services/EmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.DataAccess;
+using WebAPI.Exceptions;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(Employee EmpID);
+        Employee Create(Employee EmpID);
+        void Update(Employee EmpID);
+        void Delete(Employee EmpID);
+    }
+    public class EmployeeService
+    {
+
+    }
+}
